768387c3960666006efd35af6b3d69b01d8fdbd9
chermat mustapha <mchermat@outlook.fr>
2025-09-02 09:49:26 +0200

    change {} to %s

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,9 +8,8 @@ else:
     last = -(abs(number) % 10)
 
 if last > 5:
-    print("last digit of {} is {} and is greater than 5".format(number, last))
+    print("last digit of %s is %s and is greater than 5" %(number, last))
 elif last == 0:
-    print("last digit of {} is {} and is 0".format(number, last))
+    print("last digit of %s is %s and is 0"% (number, last))
 else:
-    print("last digit of {} is {} and is less than 6 and not 0"
-          .format(number, last))
+    print("last digit of %s is %s and is less than 6 and not 0" %(number, last))
