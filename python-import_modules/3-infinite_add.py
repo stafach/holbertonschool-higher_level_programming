9b73102cf1ec21c022a77eb72f53e6d975f03145
chermat mustapha <mchermat@outlook.fr>
2025-09-03 12:43:16 +0200

    add condition for not be executed when imported

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import sys
-x = 0
-for i in range(1, len(sys.argv)):
-    x += int(sys.argv[i])
-print("{}".format(x))
+
+if __name__ == "__main__":
+    x = 0
+    for i in range(1, len(sys.argv)):
+        x += int(sys.argv[i])
+    print("{}".format(x))
