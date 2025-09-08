81d02c08cc607a478ac4d019efe2beb4b6d137ae
chermat mustapha <mchermat@outlook.fr>
2025-09-08 13:35:06 +0200

    add format to print

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for cle in sorted(a_dictionary):
-        print(cle, a_dictionary[cle])
+        print("{:s}: {}".format(cle, a_dictionary[cle]))
