672af840c1c380cfe649a0a36ce2b0ab7b307f08
chermat mustapha <mchermat@outlook.fr>
2025-09-08 12:13:46 +0200

    add 0 in condition

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new = my_list.copy()
-    if idx < len(my_list) and idx > 0:
+    if idx < len(my_list) and idx >= 0:
         new[idx] = element
     return new
