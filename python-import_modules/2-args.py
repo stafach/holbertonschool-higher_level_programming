369875edc2d3fa5def3995176c39b5c51338f4af
chermat mustapha <mchermat@outlook.fr>
2025-09-08 09:26:31 +0200

    delete one line

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,8 +7,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif i == 2:
         print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(i - 1))
-        for x in range(1, i):
-            print("{}: {}".format(x, sys.argv[x]))
+    for x in range(1, i):
+        print("{}: {}".format(x, sys.argv[x]))
