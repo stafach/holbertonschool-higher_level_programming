c7a22634ed6d6a50816757b53a734783fe58d2f6
chermat mustapha <mchermat@outlook.fr>
2025-09-08 12:29:42 +0200

    change the function for use :d in format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print("{:d}".format(row))
+        for i, value in enumerate(row):
+            if i != 0:
+                print(" ", end="")
+            print("{:d}".format(value), end="")
+        print()
