8a192f7aa663536902d2117cca9bf50e30ff0d27
chermat mustapha <mchermat@outlook.fr>
2025-09-15 18:50:45 +0200

    create Public instance method: def area(self): that returns the current square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module create a classe named class
+"""
+
+
+class Square:
+    """
+create a square
+    """
+    def __init__(self, size=0):
+        """
+        initialize a square with a private size
+
+        Args:
+            self: the square
+            size (int): the size of the square
+
+        Raises:
+            TypeError: if size isn't an integer
+            ValueError: if size is negative
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+
+    def area(self):
+        """
+        initialize a square with a private size
+
+        Args:
+            self: the square
+
+        Return:
+            The area of the square
+            """
+        return self._Square__size ** 2
