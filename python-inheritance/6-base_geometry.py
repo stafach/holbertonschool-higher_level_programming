577137af7b3e07f5a2083afe1b2d17512e59dc1e
chermat mustapha <mchermat@outlook.fr>
2025-09-22 12:19:29 +0200

    add def area(self): to BaseGeometry class

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module create a classe named BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    class named BaseGeometry
+    """
+    def area(self):
+        """
+        calculate the area
+        """
+        raise Exception("area() is not implemented")
