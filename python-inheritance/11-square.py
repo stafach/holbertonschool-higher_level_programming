5ac2d442985ef276ac645d55b77f1d1f558b1da8
chermat mustapha <mchermat@outlook.fr>
2025-09-22 13:42:08 +0200

    add str to Square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module creates a Square class that inherits from Rectangle.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+    """
+    def __init__(self, size):
+        """
+        Args:
+            size (int): size of the square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        calculate the area
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Return the string :"[Square] <width>/<height>"
+        """
+        str = "[Square] {}/{}".format(self.__size, self.__size)
+        return str
