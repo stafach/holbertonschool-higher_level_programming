3236cfb1d0920612317921bb9e3d9d02c04db4b8
chermat mustapha <mchermat@outlook.fr>
2025-09-30 12:01:06 +0200

    add condition for to_json methode

---
+++ python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module define a class named Student
+"""
+
+
+class Student:
+    """
+    class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Args:
+            first_name: the first name of the student
+            last_name: the last name of the student
+            age: the age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation
+        of a Student instance
+        """
+        if isinstance(attrs, list):
+            for string in attrs:
+                if isinstance(string, str):
+                    dic = {}
+                    for key in attrs:
+                        if hasattr(self, key):
+                            dic[key] = getattr(self, key)
+            return dic
+        else:
+            return self.__dict__
