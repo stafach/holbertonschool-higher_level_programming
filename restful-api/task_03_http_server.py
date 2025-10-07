58f400df259d9b56a18d3cd2148e13421dba313d
chermat mustapha <mchermat@outlook.fr>
2025-10-07 16:31:07 +0200

    add /data and /status

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -6,14 +6,39 @@ this module use http.server module for implementing web servers
 
 from http.server import BaseHTTPRequestHandler
 import socketserver
+import json
 
 
 class my_server(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(b"Hello, this is a simple API!")
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            sample_data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+            reponse = json.dumps(sample_data)
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(reponse.encode("utf-8"))
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"page not found sorry")
 
 
 PORT = 8000
