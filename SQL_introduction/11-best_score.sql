75505e21c50df14c90572541ba914276f7d0c061
chermat mustapha <mchermat@outlook.fr>
2025-10-20 16:35:43 +0200

    change the place of ;

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- Script that that lists all records with score >= 10 of the table second_table
-SELECT score, name FROM second_table WHERE score >= 10;
-ORDER BY score DESC
+SELECT score, name FROM second_table WHERE score >= 10
+ORDER BY score DESC;
