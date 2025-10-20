55d5be9aa7b32a5720570cb6e2eb77b59a1a545f
chermat mustapha <mchermat@outlook.fr>
2025-10-20 16:56:43 +0200

    del ;

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 --  Script that lists all records of the table second_table except rows where the name column does not contain a value
-SELECT score, name FROM second_table WHERE name IS NOT NULL;
+SELECT score, name FROM second_table WHERE name IS NOT NULL
 ORDER BY score DESC;
