f9fa5acaf295dfc28b711393eb27e9801641fd5a
chermat mustapha <mchermat@outlook.fr>
2025-10-22 22:56:01 +0200

    change indentation

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,12 +9,12 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-    )
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
     
     cursor = db.cursor()
 
