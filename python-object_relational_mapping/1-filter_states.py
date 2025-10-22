3a994e1817b2123bdd76d969ce5395116f9f7cb2
chermat mustapha <mchermat@outlook.fr>
2025-10-22 23:01:21 +0200

    change syntax again and add states.

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,12 +18,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute(
-        "SELECT * "
-        "FROM states "
-        "WHERE name LIKE 'N%' "
-        "ORDER BY states.id"
-    )
+    cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC")
 
     for row in cursor.fetchall():
         print(row)
