a64d7a64367bf92f707c031dabd4bcfc7d51f0ce
chermat mustapha <mchermat@outlook.fr>
2025-10-22 23:17:54 +0200

    del states.

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
 
     for row in cursor.fetchall():
         print(row)
