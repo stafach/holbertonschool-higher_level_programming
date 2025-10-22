b6a0c2e9f4c8f6f976bfdd0493b92b5fc47f5458
chermat mustapha <mchermat@outlook.fr>
2025-10-22 23:20:06 +0200

    add , for tuple

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
 
     for row in cursor.fetchall():
         print(row)
