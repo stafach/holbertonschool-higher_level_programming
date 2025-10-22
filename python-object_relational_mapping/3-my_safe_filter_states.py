0f3fadfe848929ed020dc88de7dd5ababeb6bfd4
chermat mustapha <mchermat@outlook.fr>
2025-10-22 23:17:32 +0200

    write a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, write one that is safe from MySQL injections!
    Your script should take 4 arguments: mysql username, mysql password, database name and state name searched (safe from MySQL injection)
    You must use the module MySQLdb (import MySQLdb)
    Your script should connect to a MySQL server running on localhost at port 3306
    Results must be sorted in ascending order by states.id
    Results must be displayed as they are in the example below
    Your code should not be executed when imported

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where
+name matches the argument but safe.
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC", (sys.argv[4]))
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
