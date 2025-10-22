3e5216cae3877e3158261371e43b301739cf7969
chermat mustapha <mchermat@outlook.fr>
2025-10-22 23:34:02 +0200

    Write a script that lists all cities from the database hbtn_0e_4_usa
    Your script should take 3 arguments: mysql username, mysql password and database name
    You must use the module MySQLdb (import MySQLdb)
    Your script should connect to a MySQL server running on localhost at port 3306
    Results must be sorted in ascending order by cities.id
    You can use only execute() once
    Results must be displayed as they are in the example below
    Your code should not be executed when imported

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where
+name matches the argument but safe.
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities "
+        "JOIN states "
+        "ON cities.state_id = states.id "
+        "ORDED BY cities.id ASC"
+    )
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close
+    db.close
