060d81445c8bc6fc7d787804e2559c430c30b43e
chermat mustapha <mchermat@outlook.fr>
2025-10-23 15:57:48 +0200

    Next, write a script 14-model_city_fetch_by_state.py that prints all City objects from the database hbtn_0e_14_usa:
    Your script should take 3 arguments: mysql username, mysql password and database name
    You must use the module SQLAlchemy
    You must import State and Base from model_state - from model_state import Base, State
    Your script should connect to a MySQL server running on localhost at port 3306
    Results must be sorted in ascending order by cities.id
    Results must be display as they are in the example below (<state name>: (<city id>) <city name>)
    Your code should not be executed when imported

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+script that prints all City
+objects from the database hbtn_0e_14_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    results = (
+        session.query(City, State)
+        .join(State, City.state_id == State.id)
+        .all()
+    )
+
+    for city, state in results:
+        print("{}: ({}) {}" .format(state.name, city.id, city.name))
