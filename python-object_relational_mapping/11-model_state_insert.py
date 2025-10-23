2473fc55cb1ee2d34bac12a4a81cf4f072f78173
chermat mustapha <mchermat@outlook.fr>
2025-10-23 14:54:10 +0200

    del import

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -8,9 +8,7 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-import pymysql
 
-pymysql.install_as_MySQLdb()
 
 if __name__ == "__main__":
     engine = create_engine(
