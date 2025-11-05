c6e89012da3d2f1d73dac3b1fbfd71add17f65c6
chermat mustapha <mchermat@outlook.fr>
2025-11-05 11:26:23 +0100

    add space after else

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,6 +4,6 @@ if (process.argv.length === 2) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
-} else {
+} else {
   console.log('Arguments found');
 }
