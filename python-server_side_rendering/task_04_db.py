df86e6fab26b2d4bca8ea379f06a05db5894d460
chermat mustapha <mchermat@outlook.fr>
2025-11-12 22:12:16 +0100

    add sqlite3 for products

---
+++ python-server_side_rendering/task_04_db.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request
+import json
+import csv
+import sqlite3
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    with open("items.json", "r") as f:
+        data = json.load(f)
+    return render_template('items.html', items=data["items"])
+
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    products_id = request.args.get('id')
+    
+    if source == 'json':
+        with open('products.json') as f:
+            data = json.load(f)
+
+    elif source == 'csv':
+        with open('products.csv') as f:
+            reader = csv.DictReader(f)
+            data = list(reader)
+
+    elif source == 'sql':
+        try:
+            conn = sqlite3.connect('products.db')
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM Products")
+            data = cur.fetchall()
+            data = [{'id': row[0], 'name': row[1], 'category': row[2], 'price': row[3]} for row in data]
+            conn.close()
+        except sqlite3.Error as e:
+            return render_template('product_display.html', error="Database error: {}".format(e))
+
+    else:
+        return render_template('product_display.html', error="Wrong source")
+    
+    if products_id:
+        data = [product for product in data if str(product['id']) == str(products_id)]
+        if not data:
+            error = "Product not found"
+            return render_template('product_display.html', error=error)
+        
+    return render_template('product_display.html', products=data)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
